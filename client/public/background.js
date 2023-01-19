014b3a15cc95ef88b5cf54dbc6467735b0ef178d
rubil chaudhary <rubil@applicatetechnology.com>
2023-01-19 22:17:21 +0530

    updated with react code

--- client/public/background.js
+++ client/public/background.js
@@ -1,8 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId,tab) => {
-    if(tab.url && (tab.url.includes("internshala") || tab.url.includes("meet.google.com"))) {
         console.log("background open")
         chrome.tabs.sendMessage(tabId, {
             type: "Open"
         })
-    }
 })
