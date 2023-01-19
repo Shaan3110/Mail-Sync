7772e1acb6798ff4128d68dcfb381a3401bbd5a6
rubil chaudhary <rubil@applicatetechnology.com>
2023-01-19 19:03:38 +0530

    last changes -- one small bug

--- client/background.js
+++ client/background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId,tab) => {
     if(tab.url && (tab.url.includes("internshala") || tab.url.includes("meet.google.com"))) {
-        
+        console.log("background open")
+        chrome.tabs.sendMessage(tabId, {
+            type: "Open"
+        })
     }
 })
--- client/contentScript.js
+++ client/contentScript.js
@@ -1,3 +1,9 @@
 (()=> {
+    chrome.runtime.onMessage.addListener((obj,sender,response) => {
+        const { type } = obj;
 
+        if ( type === "OPEN") {
+            console.log(sender);
+        }
+    })
 })();
